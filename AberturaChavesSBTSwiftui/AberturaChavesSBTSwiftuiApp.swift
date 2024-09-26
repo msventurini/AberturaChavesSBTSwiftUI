2874f44aef4e9de2051ae08022e02c19b0182724
msventurini <42521920+msventurini@users.noreply.github.com>
2024-09-26 13:17:02 -0300

    Initial Commit

---
+++ AberturaChavesSBTSwiftui/AberturaChavesSBTSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AberturaChavesSBTSwiftuiApp.swift
+//  AberturaChavesSBTSwiftui
+//
+//  Created by Matheus Silveira Venturini on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AberturaChavesSBTSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AberturaChavesSBTSwiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AberturaChavesSBTSwiftui
+//
+//  Created by Matheus Silveira Venturini on 26/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
