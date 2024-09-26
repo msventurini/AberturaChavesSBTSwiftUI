4bdf926a3287c74fcf742c23664e5c936729f0d8
msventurini <42521920+msventurini@users.noreply.github.com>
2024-09-26 15:14:21 -0300

    added basic background template

--- AberturaChavesSBTSwiftui/AberturaChavesSBTSwiftuiApp.swift
+++ AberturaChavesSBTSwiftui/AberturaChavesSBTSwiftuiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AberturaChavesSBTSwiftuiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BackgroundView()
         }
     }
 }
--- AberturaChavesSBTSwiftui/BackgroundView.swift
+++ AberturaChavesSBTSwiftui/BackgroundView.swift
@@ -10,40 +10,54 @@ import SwiftUI
 struct BackgroundView: View {
     var body: some View {
         
-        Canvas(opaque: true) { context, size in
-            
-            let firstQuadrantSquare = CGRect(
-                origin: CGPointMake(size.width * 0.5, .zero),
-                size: CGSizeMake(size.width * 0.5, size.height * 0.5)
-            )
-            
-            let secondQuadrantSquare = CGRect(
-                origin: CGPointMake(.zero, .zero),
-                size: CGSizeMake(size.width * 0.5, size.height * 0.5)
-            )
-            
-            let thirdQuadrantSquare = CGRect(
-                origin: CGPointMake(.zero, size.height * 0.5),
-                size: CGSizeMake(size.width * 0.5, size.height * 0.5)
-            )
-            
-            let fourthQuadrantSquare = CGRect(
-                origin: CGPointMake(size.width * 0.5, size.height * 0.5),
-                size: CGSizeMake(size.width * 0.5, size.height * 0.5)
-            )
-            
+//        TimelineView(.animation) { timelineContext in
+        TimelineView(.periodic(from: .now, by: 0.00015)) { timelineContext in
+            Canvas(opaque: true) { context, size in
+                
+                let time = timelineContext.date
+                
+                let center = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+                let sideSize = size.width * 2
+                
+                
+                let firstQuadrantSquare = CGRect(
+                    origin: CGPointMake(center.x, .zero),
+                    size: CGSizeMake(size.width * 0.5, size.height * 0.5)
+                )
+                
+                let secondQuadrantSquare = CGRect(
+                    origin: CGPointMake(.zero, .zero),
+                    size: CGSizeMake(size.width * 0.5, size.height * 0.5)
+                )
+                
+                let thirdQuadrantSquare = CGRect(
+                    origin: CGPointMake(.zero, center.y),
+                    size: CGSizeMake(size.width * 0.5, size.height * 0.5)
+                )
+                
+                let fourthQuadrantSquare = CGRect(
+                    origin: CGPointMake(center.x, center.y),
+                    size: CGSizeMake(size.width * 0.5, size.height * 0.5)
+                )
+                
+                context.transform = CGAffineTransform(translationX: center.x, y: center.y)
+                    .rotated(by: Angle(degrees: timelineContext.date.timeIntervalSince1970).radians * 90)
+                    .translatedBy(x: -center.x, y: -center.y)
 
-            context.fill(Rectangle().path(in: firstQuadrantSquare), with: .color(Color.Background.midLightBlue))
-            context.fill(Rectangle().path(in: secondQuadrantSquare), with: .color(Color.Background.midDarkBlue))
-            context.fill(Rectangle().path(in: thirdQuadrantSquare), with: .color(Color.Background.lightBlue))
-            
-            context.fill(Rectangle().path(in: fourthQuadrantSquare), with: .color(Color.Background.darkBlue))
-
-            
-            
+                context.fill(Rectangle().path(in: firstQuadrantSquare), with: .color(Color.Background.midLightBlue))
+                context.fill(Rectangle().path(in: secondQuadrantSquare), with: .color(Color.Background.midDarkBlue))
+                context.fill(Rectangle().path(in: thirdQuadrantSquare), with: .color(Color.Background.lightBlue))
+                context.fill(Rectangle().path(in: fourthQuadrantSquare), with: .color(Color.Background.darkBlue))
+   
+            }
+//            .frame(width: 1280)
         }
+        
+        
         .aspectRatio(1, contentMode: .fill)
-        .rotationEffect(Angle(degrees: 43))
+        .frame(width: 640, height: 480)
+        .clipped()
+        
         
     }
 }
